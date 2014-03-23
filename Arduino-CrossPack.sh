075fa603bb500ebf79437a4af4d155f6a23cf602
tinoest <tinoest@gmail.com>
2014-03-23 18:02:54 +0000

    Corrected Usage Bug, changed to a mv rather than a rm

--- Arduino-CrossPack.sh
+++ Arduino-CrossPack.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -x
 #
-# Usage: Arduino-Crosspack /Applications/Arduino.app
+# Usage: Arduino-CrossPack /Applications/Arduino.app
 #
 AVR="$1/Contents/Resources/Java/hardware/tools/avr"
+AVR="$1/Contents/Resources/Java/hardware/tools/avr-old"
  
-sudo rm -rf "$AVR"
+sudo mv -f "$AVR" "$AVRMV"
 sudo ln -s /usr/local/CrossPack-AVR "$AVR"
 sudo codesign -fs - "$1"
